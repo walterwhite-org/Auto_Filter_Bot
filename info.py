b58a76be9b1c17ae3df6d526f61a4e5cb9ffbb49
Walter 🤍 <133798451+walterwhite-org@users.noreply.github.com>
2026-04-23 23:56:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -124,7 +124,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/mallumovieport') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/hodycloud') # Owner link for the bot
-UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/newcinemaupdates') # Update channel link for the bot
+UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/+vVfNX48UKpU4ZTFl') # Update channel link for the bot
 
 # ============================
 # User Configuration
